c781340216ff847a100faf0c3fad5afe141d4fcf
sanhita2019 <54019798+sanhita2019@users.noreply.github.com>
2023-08-31 20:04:11 +0530

    Update helpers.js

--- test/helpers.js
+++ test/helpers.js
@@ -1,7 +1,218 @@
-var supertest = require('supertest');
-var chai = require('chai');
-var app = require('../server');
-
-global.app = app;
-global.expect = chai.expect;
-global.request = supertest(app);
+{
+    "programId": "",
+    "projectId": "",
+    "status": "",
+    "appType": "PreApplication",
+    "enrolment": {
+      "appType": "PreApplication",
+      "appNb": "",
+      "applicantInformation": {
+        "firstName": "Siva",
+        "lastName": "M",
+        "email": "",
+        "address1": "",
+        "address2": "",
+        "city": "NY",
+        "state": "",
+        "zipCode": "111",
+        "contactNo": "4444",
+        "mobileNo": "",
+        "companyName": "",
+        "contactType": "Customer"
+      },
+      "isApplicantChanged": true,
+      "contractor": {
+        "firstName": "",
+        "lastName": "",
+        "email": "",
+        "address1": "",
+        "address2": "",
+        "city": "",
+        "state": "",
+        "zipCode": "",
+        "contactNo": "",
+        "mobileNo": "",
+        "companyName": "",
+        "contactType": "Customer"
+      },
+      "isContractorChanged": true,
+      "customer": {
+        "customerNb": "string",
+        "accountNb": "string",
+        "premiseNb": "string",
+        "firstName": "",
+        "lastName": "",
+        "email": "",
+        "address1": "",
+        "address2": "",
+        "city": "",
+        "state": "",
+        "zipCode": "",
+        "contactNo": "",
+        "mobileNo": "",
+        "companyName": "",
+        "contactType": "Customer"
+      },
+      "isCustomerChanged": true,
+      "customerPrograms": [
+        {
+          "equipmentModule": {
+            "inverters": [
+              {
+                "hybridPVBattery": true,
+                "inverterType": "string",
+                "maxContPowerKW": 0,
+                "microInverter": true,
+                "solarModule": {
+                  "solarModules": [
+                    {
+                      "setCapQy": 0,
+                      "maxCapQy": 0,
+                      "tilt": 0,
+                      "azimuth": 0,
+                      "tracking": "string",
+                      "shading": [
+                        {
+                          "month": "string",
+                          "shadeQty": 0
+                        }
+                      ],
+                      "mfrName": "Test",
+                      "modelName": "Test",
+                      "unitQy": 10
+                    }
+                  ]
+                },
+                "batteries": [
+                  {
+                    "setCapacityKWH": 0,
+                    "maxContDischargeRateKW": 0,
+                    "mfrName": "string",
+                    "modelName": "string",
+                    "unitQy": 0
+                  }
+                ],
+                "meter": true,
+                "customerMeters": [
+                  {
+                    "meterNb": "string",
+                    "meterType": "string"
+                  }
+                ],
+                "mfrName": "string",
+                "modelName": "string",
+                "unitQy": 0
+              }
+            ],
+            "integratedEnergySystems": [
+              {
+                "pvdcInputCapability": true,
+                "setCapacityKWH": 0,
+                "maxContDischargeRateKW": 0,
+                "setPowerKw": 0,
+                "solarModule": {
+                  "solarModules": [
+                    {
+                      "setCapQy": 0,
+                      "maxCapQy": 0,
+                      "tilt": 0,
+                      "azimuth": 0,
+                      "tracking": "string",
+                      "shading": [
+                        {
+                          "month": "string",
+                          "shadeQty": 0
+                        }
+                      ],
+                      "mfrName": "string",
+                      "modelName": "string",
+                      "unitQy": 0
+                    }
+                  ]
+                },
+                "meter": true,
+                "customerMeters": [
+                  {
+                    "meterNb": "string",
+                    "meterType": "string"
+                  }
+                ],
+                "mfrName": "string",
+                "modelName": "string",
+                "unitQy": 0
+              }
+            ],
+            "otherGenerators": [
+              {
+                "fuelType": "string",
+                "genType": "string",
+                "powerType": "string",
+                "setCapacityKW": 0,
+                "maxCapacityKW": 0,
+                "minCapacityKW": 0,
+                "powerFactor": 0,
+                "meter": true,
+                "customerMeters": [
+                  {
+                    "meterNb": "string",
+                    "meterType": "string"
+                  }
+                ],
+                "mfrName": "string",
+                "modelName": "string",
+                "unitQy": 0
+              }
+            ],
+            "loadMeterNb": "string",
+            "customerSubPrograms": [
+              {
+                "programSubType": "string",
+                "typeOfMesasure": "string",
+                "contractRate": 0
+              }
+            ]
+          },
+          "owner": {
+            "firstName": "",
+            "lastName": "",
+            "email": "",
+            "address1": "",
+            "address2": "",
+            "city": "",
+            "state": "",
+            "zipCode": "",
+            "contactNo": "",
+            "mobileNo": "",
+            "companyName": "",
+            "contactType": "Customer"
+          },
+          "aggregator": {
+            "firstName": "",
+            "lastName": "",
+            "email": "",
+            "address1": "",
+            "address2": "",
+            "city": "",
+            "state": "",
+            "zipCode": "",
+            "contactNo": "",
+            "mobileNo": "",
+            "companyName": "",
+            "contactType": "Customer"
+          },
+          "programType": "Net_Metering",
+          "programName": "string",
+          "systemSize": 0
+        }
+      ],
+      "areProgramsChanged": true,
+      "attachments": [
+        {
+          "attachementName": "",
+          "attachementType": "SiteDiagrams",
+          "attachement": "",
+          "attachementId": ""
+        }
+      ]
+    }
+  }
